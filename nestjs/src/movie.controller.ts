85ee2c5baacd2df1abe3a1020891e7d0ee2436a8
David Lay <davidlaym@gmail.com>
2021-06-03 18:14:02 -0400

    fixed dependency injection

--- nestjs/src/movie.controller.ts
+++ nestjs/src/movie.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { MoviesFeatures } from './features/movie.features';
 
 @Controller('movie')
 export class MovieController {
-  constructor(@Inject() private readonly movieService: MoviesFeatures) {}
+  constructor(private readonly movieService: MoviesFeatures) {}
   @Get()
   getAll(): Record<string, unknown>[] {
     return this.movieService.getAll() as unknown as Record<string, unknown>[];
